fc9f133d7819f2bf3b1b4e3d866e60ddf07395c0
thanhhuynhk17 <csk17fit@gmail.com>
2020-12-09 14:39:48 +0700

    ui-dev & main merged

---
+++ WeSplit/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WeSplit.ViewModel
+{
+    public class MainViewModel: BaseViewModel
+    {
+        // Constructor
+        bool isLoaded = false;
+        public MainViewModel()
+        {
+            isLoaded = true;
+            if (!isLoaded)
+            {
+                MessageBox.Show($"{isLoaded} boolean value");
+            }
+        }
+    }
+}
