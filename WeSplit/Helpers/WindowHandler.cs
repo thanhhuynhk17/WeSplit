0fe59b7a314e290746dfeb97baa0f0a91e65996d
thanhhuynhk17 <csk17fit@gmail.com>
2020-12-12 00:23:05 +0700

    add helper funtions: find parent tag

---
+++ WeSplit/Helpers/WindowHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WeSplit.Helpers
+{
+    public static class WindowHandler
+    {
+
+        /// <summary>
+        /// Get real parent of UserControl p
+        /// </summary>
+        /// <param name="p">UserControl</param>
+        /// <returns>real parent of p</returns>
+        public static FrameworkElement GetParentWindow(UserControl p)
+        {
+            FrameworkElement parent = p;
+
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+
+            return parent;
+        }
+
+    }
+}
