ea9a935227560f8471376f7d93c308e70ea69990
Ngo Van Tai <internit00@okxe.vn>
2020-12-12 15:22:47 +0700

    oke

--- WeSplit/ViewModel/MainViewModel.cs
+++ WeSplit/ViewModel/MainViewModel.cs
@@ -5,11 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace WeSplit.ViewModel
 {
     public class MainViewModel: BaseViewModel
     {
-
+        public ICommand AddJourneyCommand { get; set; }
+        
+        public MainViewModel()
+        {
+            AddJourneyCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                SplashScreenWindow splash = new SplashScreenWindow();
+                splash.ShowDialog();
+            });
+        }
     }
 }
